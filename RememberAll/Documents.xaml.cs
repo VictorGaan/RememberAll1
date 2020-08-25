d919784e17dad4ecf1fe991d86198b856d3872ef
Victor <gaan_v@bk.ru>
2020-08-25 21:08:17 +0700

    25.08

--- RememberAll/Documents.xaml.cs
+++ RememberAll/Documents.xaml.cs
@@ -1,21 +1,14 @@
-﻿using Microsoft.Office.Interop.Excel;
+﻿using Microsoft.Office.Interop.Word;
 using Microsoft.Win32;
-using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using Excel = Microsoft.Office.Interop.Excel;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace RememberAll
 {
@@ -33,9 +26,10 @@ namespace RememberAll
         public Documents()
         {
             InitializeComponent();
-            RememberAllEntities db = new RememberAllEntities();
+            BagListView.Background = new SolidColorBrush(Properties.Settings.Default.MyColor);
             ProductListView.ItemsSource = db.Users.ToList();
         }
+        RememberAllEntities db = new RememberAllEntities();
 
         List<Product> products = new List<Product>();
         private void MenuItemOpen_Click(object sender, RoutedEventArgs e)
@@ -45,7 +39,7 @@ namespace RememberAll
             {
                 Excel.Application app = new Excel.Application();
                 Excel.Workbook workbook = app.Workbooks.Open(openFile.FileName);
-                Excel.Worksheet worksheet = workbook.Sheets[1];
+                Excel.Worksheet worksheet = app.Worksheets[1];
 
                 int rows = worksheet.UsedRange.Rows.Count;
                 //int columns = worksheet.UsedRange.Columns.Count;
@@ -58,12 +52,14 @@ namespace RememberAll
                     //}
                     Product product = new Product
                     {
-                        Count = ((Range)worksheet.Cells[i, 1]).Value.ToString(),
-                        Name = ((Range)worksheet.Cells[i, 2]).Value.ToString(),
+                        Count = ((Excel.Range)worksheet.Cells[i, 1]).Value.ToString(),
+                        Name = ((Excel.Range)worksheet.Cells[i, 2]).Value.ToString(),
                     };
                     products.Add(product);
 
                 }
+                workbook.Close();
+                app.Quit();
             }
             foreach (var item in products)
             {
@@ -77,22 +73,72 @@ namespace RememberAll
         }
         private void MenuItemExcel_Click(object sender, RoutedEventArgs e)
         {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.FileName = "report";
+            saveFile.Filter = "Excel files |*.xlsx";
+            if (saveFile.ShowDialog() == true)
+            {
+                Excel.Application app = new Excel.Application();
+                Excel.Workbook workbook = app.Workbooks.Add();
+                Excel.Worksheet worksheet = app.Worksheets[1];
 
+                worksheet.Range["A1"].Value = "First Name";
+                worksheet.Range["B1"].Value = "Last Name";
+                worksheet.Range["C1"].Value = "Middle Name";
+
+                int i = 1;
+                foreach (var item in db.Users.ToList())
+                {
+                    i++;
+                    worksheet.Range[$"A{i}"].Value = item.FirstName;
+                    worksheet.Range[$"B{i}"].Value = item.LastName;
+                    worksheet.Range[$"C{i}"].Value = item.MiddleName;
+                }
+                workbook.SaveAs(saveFile.FileName);
+                workbook.Close();
+                app.Quit();
+            }
         }
 
         private void MenuItemWord_Click(object sender, RoutedEventArgs e)
         {
-
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.FileName = "report";
+            saveFile.Filter = "Docx files |*.docx";
+            if (saveFile.ShowDialog() == true)
+            {
+                Word.Application app = new Word.Application();
+                Word.Document doc = app.Documents.Add();
+                Word.Paragraph paragraph = doc.Paragraphs.Add();
+                paragraph.Range.Text = "Sample text\t tabulation\n" +
+                    "New stroke\t tabulation\n";
+                doc.SaveAs2(saveFile.FileName);
+                doc.Close();
+                app.Quit();
+            }
         }
 
         private void MenuItemPdf_Click(object sender, RoutedEventArgs e)
         {
-
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.FileName = "report";
+            saveFile.Filter = "Pdf files |*.pdf";
+            if (saveFile.ShowDialog() == true)
+            {
+                Word.Application app = new Word.Application();
+                Word.Document doc = app.Documents.Add();
+                Word.Paragraph paragraph = doc.Paragraphs.Add();
+                paragraph.Range.Text = "Sample text\t tabulation\n" +
+                    "New stroke\t tabulation\n";
+                doc.SaveAs2(saveFile.FileName, WdSaveFormat.wdFormatPDF);
+                doc.Close();
+                app.Quit();
+            }
         }
 
 
 
-        private void BagListView_Drop(object sender, DragEventArgs e)
+        private void BagListView_Drop(object sender, System.Windows.DragEventArgs e)
         {
             Users user = (Users)e.Data.GetData(e.Data.GetFormats()[0]);
             foreach (Users item in BagListView.Items)
@@ -116,6 +162,54 @@ namespace RememberAll
             {
                 DragDrop.DoDragDrop(listView, listView.SelectedItem as Users, DragDropEffects.Copy);
             }
+        }
+
+        private void MenuItemColor_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.ColorDialog colorDialog = new System.Windows.Forms.ColorDialog();
+
+            if (colorDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var argb = colorDialog.Color;
+                BagListView.Background = new SolidColorBrush(System.Windows.Media.Color.FromArgb(argb.A, argb.R, argb.G, argb.B));
+                Properties.Settings.Default.MyColor = System.Windows.Media.Color.FromArgb(argb.A, argb.R, argb.G, argb.B);
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void MenuItemFolder_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog folderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            if (folderBrowser.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                MessageBox.Show(folderBrowser.SelectedPath);
+            }
+        }
+
+        private void MenuItemPrint_Click(object sender, RoutedEventArgs e)
+        {
+            PrintDialog printDialog = new PrintDialog();
+            if (printDialog.ShowDialog() == true)
+            {
+
+                TextBlock textBlock = new TextBlock
+                {
+                    Text = "sample text to print",
+                    TextWrapping = TextWrapping.Wrap,
+                };
+                printDialog.PrintVisual(textBlock, "ListView");
+            }
+
+        }
+
+        private void MenuItemRead_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemWrite_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
